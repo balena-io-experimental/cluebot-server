06761d254e8f97eafaf9422b94673874c5a8acf1
Christina Wang <christina@balena.io>
2021-11-08 00:40:36 -0800

    Remove leave/join functionality
    We don't need this functionality, as cluebot should just
    ping Flowdock's @team, and people who wish to not get
    pings should manage this through Flowdock.
    
    Signed-off-by: Christina Wang <christina@balena.io>

--- server/index.ts
+++ server/index.ts
@@ -45,6 +45,7 @@ app.get('/api/players', async (_req, res) => {
 });
 
 app.post('/api/answer', async (req, res) => {
+	// Validate request body
 	const { handle, answer } = req.body;
 
 	if (typeof handle !== 'string') {
@@ -59,9 +60,7 @@ app.post('/api/answer', async (req, res) => {
 	}
 
 	try {
-		// Upsert db entry with is_playing: true.
-		// If player exists, update is_playing on their existing entry.
-		// If player doesn't exist, create entry in players table with handle and is_playing.
+		// If player doesn't exist, create entry in players table with handle
 		await db.setOrUpdateAnswerForPlayer({ handle, answer });
 		res
 			.status(200)
@@ -116,55 +115,6 @@ app.get('/api/answers', async (_req, res) => {
 	} catch (e) {
 		console.error(e);
 		res.status(500).json({ error: `Internal server error: ${e}` });
-	}
-});
-
-app.post('/api/join', async (req, res) => {
-	// Validate POST req input
-	const { handle } = req.body;
-	if (typeof handle !== 'string') {
-		return res.status(400).json({
-			error: `Invalid handle in POST request. Expected a string, got: '${handle}'`,
-		});
-	}
-
-	try {
-		// Upsert db entry with is_playing: true.
-		// If player exists, update is_playing on their existing entry.
-		// If player doesn't exist, create entry in players table with handle and is_playing
-		await db.addOrUpdatePlayer({ handle, is_playing: true });
-		res.status(200).json({
-			message: `User with handle '${handle}' successfully joined game`,
-		});
-	} catch (e) {
-		console.error(e);
-		res.status(500).json({
-			error: `Internal server error while adding or updating player entry: ${e}`,
-		});
-	}
-});
-
-app.post('/api/leave', async (req, res) => {
-	// Validate POST req input
-	const { handle } = req.body;
-	if (typeof handle !== 'string') {
-		return res.status(400).json({
-			error: `Invalid handle in POST request. Expected a string, got: '${handle}'`,
-		});
-	}
-
-	// Set player to inactive status by changing the is_playing property to false.
-	// The player's handle remains in the db for when they decide to join again.
-	try {
-		await db.addOrUpdatePlayer({ handle, is_playing: false });
-		res.status(200).json({
-			message: `User with handle '${handle}' successfully deleted from game`,
-		});
-	} catch (e) {
-		console.error(e);
-		res.status(500).json({
-			error: `Internal server error while updating player entry: ${e}`,
-		});
 	}
 });
 
